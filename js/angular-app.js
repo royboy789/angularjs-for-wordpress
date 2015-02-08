7be52e5c9e2163333965c246634113cbb0d429ad
Roman <w2@live.ru>
2015-02-08 03:41:04 +0200

    Fixed loading with angular

--- js/angular-app.js
+++ js/angular-app.js
@@ -6,7 +6,26 @@ app.filter('unsafe', function($sce) {
         return $sce.trustAsHtml(val);
     };
 });
+app.directive('ngRender', ['$compile', function ($compile) {
+    return {
+      restrict: 'E',
+      scope: {
+        html: '='
+      },
+      link: function postLink(scope, element, attrs) {
 
+          function appendHtml() {
+              if(scope.html) {
+                  var newElement = angular.element(scope.html);
+                  $compile(newElement)(scope);
+                  element.append(newElement);
+              }
+          }
+
+          scope.$watch(function() { return scope.html }, appendHtml);
+      }
+    };
+  }]);
 app.run( function( $rootScope, $http, $sce ) {
 	$http.get(wpAngularVars.base + '/posts').then(function(res){
 		$rootScope.posts = res.data;
