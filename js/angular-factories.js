13dd906c05b3ceeec3b2b1cdcf61ebc2ec390496
Josh Pollock <jpollock412@gmail.com>
2015-11-12 19:44:58 -0500

    Update nonce for v2

--- js/angular-factories.js
+++ js/angular-factories.js
@@ -1,5 +1,5 @@
 angular_app.factory('Posts', function($resource){
-	return $resource(wpAngularVars.base +'/posts/:id?_wp_json_nonce=' + wpAngularVars.nonce, {id: '@id'}, {
+	return $resource(wpAngularVars.base +'/posts/:id?_wp_rest_nonce=' + wpAngularVars.nonce, {id: '@id'}, {
 		update: {method: 'PUT'}
 	});
 });
